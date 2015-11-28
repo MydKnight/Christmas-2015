5aba4258f79b995e56fa3b10e4b4d170519331d9
madsens <shiloh.madsen@gmail.com>
2015-11-28 15:14:58 -0800

    Reuploading movie Loop

--- Snowfall.py
+++ Snowfall.py
@@ -2,14 +2,14 @@ __author__ = 'madsens'
 import Movies
 
 print 'Starting'
-#Movies.StartLoop('/home/pi/Christmas-2015/Assets/Snowfall')
+Movies.StartLoop('/home/pi/Christmas-2015/Assets/Snowfall')
 
 while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
     n = raw_input("Scanned ID: ")
     if n == "STOP":
-        #Movies.StopLoop()
+        Movies.StopLoop()
         print "Stopping."
         break  # stops the loop
     else :
         print "Playing."
-        #Movies.PlayMovie()
+        Movies.PlayMovie()
