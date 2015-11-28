2dbf86db28df48cc97194a2263715d497025af6d
madsens <shiloh.madsen@gmail.com>
2015-11-28 14:22:39 -0800

    Updating loop script

--- 2 - Snowfall.py
+++ 2 - Snowfall.py
@@ -3,3 +3,11 @@ import Movies
 
 print 'Starting'
 Movies.StartLoop('/home/pi/Christmas-2015/Assets/Snowfall')
+
+while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
+    n = raw_input("Scanned ID: ")
+    if n == "STOP":
+        Movies.StopLoop()
+        break  # stops the loop
+    else :
+        Movies.PlayMovie()
