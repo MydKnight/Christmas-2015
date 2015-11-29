83ae862e90ab6a1346c390f392ee9283a1b9e832
madsens <shiloh.madsen@gmail.com>
2015-11-28 20:27:17 -0800

    Still trying ot make background transparent

--- NaughtyNice.py
+++ NaughtyNice.py
@@ -35,7 +35,7 @@ while True:
         name = rfid
 
         # Fadein Name of scanned ID
-        label = font.render(name,1,( ))
+        label = font.render(name,1)
         newSurf = pygame.Surface(font.size(name))
         newSurf.blit(label,(0,0))
         for x in range (225):
