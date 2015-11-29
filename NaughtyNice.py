1cc200c27edfc147f1fad91b815b522adc5ac32f
madsens <shiloh.madsen@gmail.com>
2015-11-28 20:22:55 -0800

    Turning alpha off for now.

--- NaughtyNice.py
+++ NaughtyNice.py
@@ -39,7 +39,7 @@ while True:
         newSurf = pygame.Surface(font.size(name))
         newSurf.blit(label,(0,0))
         for x in range (225):
-            newSurf.set_alpha(x - 255)
+            #newSurf.set_alpha(x - 255)
             screen.blit(newSurf, (100,100))
             pygame.display.flip()
             pygame.time.delay(20)
