f00822a24d32de15aa40bb9f901d4f1d71b0b296
madsens <shiloh.madsen@gmail.com>
2015-11-28 17:20:36 -0800

    Path Change

--- SantasWindow.py
+++ SantasWindow.py
@@ -2,10 +2,9 @@ __author__ = 'madsens'
 import Movies
 import datetime
 import time
-from time import sleep
 
 print 'Starting'
-Movies.StartLoop('/home/pi/Halloween2015/Assets/DayLoop')
+Movies.StartLoop('/home/pi/Christmas-2015/Assets/DayLoop')
 
 while True:
     now = datetime.datetime.now()
@@ -14,10 +13,10 @@ while True:
     if datetime.time(6,00) <= now.time() <= datetime.time(17,00):
         print "Its Daytime"
         Movies.StopLoop()
-        Movies.StartLoop('/home/pi/Halloween2015/Assets/DayLoop')
+        Movies.StartLoop('/home/pi/Christmas-2015/Assets/DayLoop')
     else:
         print "Its Night Time"
         Movies.StopLoop()
-        Movies.StartLoop('/home/pi/Halloween2015/Assets/NightLoop')
+        Movies.StartLoop('/home/pi/Christmas-2015/Assets/NightLoop')
     print "Sleeping for one hour"
     time.sleep(3600)
