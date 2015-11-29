c3d72495ee747a52d64a469b184b59cbfeea2220
madsens <shiloh.madsen@gmail.com>
2015-11-28 16:53:56 -0800

    Commenting out movies to make sure time check works

--- Santas Window.py
+++ Santas Window.py
@@ -3,7 +3,7 @@ import Movies
 from datetime import datetime, time
 
 print 'Starting'
-Movies.StartLoop('/home/pi/Halloween2015/Assets/LivingLogo')
+#Movies.StartLoop('/home/pi/Halloween2015/Assets/LivingLogo')
 
 while True:
     now = datetime.now()
@@ -11,11 +11,11 @@ while True:
     #If between 6AM and 5PM, Play Daytime
     if time(6,00) <= now.time() <= time(17,00):
         print "Its Daytime"
-        Movies.StopLoop()
-        Movies.StartLoop('/home/pi/Halloween2015/Assets/DayLoop')
+        #Movies.StopLoop()
+        #Movies.StartLoop('/home/pi/Halloween2015/Assets/DayLoop')
     else:
         print "Its Night Time"
-        Movies.StopLoop()
-        Movies.StartLoop('/home/pi/Halloween2015/Assets/NightLoop')
+        #Movies.StopLoop()
+        #Movies.StartLoop('/home/pi/Halloween2015/Assets/NightLoop')
     print "Sleeping for one hour"
     time.sleep(3600)
