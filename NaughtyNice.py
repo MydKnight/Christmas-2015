9331aa27b8e824d204247fc05c6b81258ca16994
madsens <shiloh.madsen@gmail.com>
2015-11-28 21:19:51 -0800

    Trying setting alpha directly - 1

--- NaughtyNice.py
+++ NaughtyNice.py
@@ -39,13 +39,13 @@ while True:
         # Display some text
         font = pygame.font.Font(None, 36)
         text = font.render(name, 1, (10, 10, 10))
-        text.set_alpha(100)
+        text.set_alpha(10)
         textpos = text.get_rect()
         textpos.centerx = screen.get_rect().centerx
         screen.blit(text, textpos)
         pygame.display.flip()
 
-        for x in range (len(name)):
+        for x in range (255):
             text.set_alpha(x)
             print x
             screen.blit(text, textpos)
