7d2bd0dfbe3ca97dd8f6d9a0d2ad30c939fc0770
madsens <shiloh.madsen@gmail.com>
2015-11-28 17:05:39 -0800

    Still tyring to fix namespace6

--- SantasWindow.py
+++ SantasWindow.py
@@ -1,16 +1,15 @@
 __author__ = 'madsens'
 import Movies
-import time
-import datetime
+from datetime import datetime, time
 
 print 'Starting'
 #Movies.StartLoop('/home/pi/Halloween2015/Assets/LivingLogo')
 
 while True:
-    now = datetime.datetime.now()
-    now_time = datetime.now.time()
+    now = datetime.now()
+    now_time = now.time()
     #If between 6AM and 5PM, Play Daytime
-    if time(6,00) <= datetime.now.time() <= time(17,00):
+    if time(6,00) <= now.time() <= time(17,00):
         print "Its Daytime"
         #Movies.StopLoop()
         #Movies.StartLoop('/home/pi/Halloween2015/Assets/DayLoop')
@@ -19,4 +18,3 @@ while True:
         #Movies.StopLoop()
         #Movies.StartLoop('/home/pi/Halloween2015/Assets/NightLoop')
     print "Sleeping for one hour"
-    time.sleep(3600)
