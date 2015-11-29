4eb044e44d40cd79495aedae6cca06a41a91ee13
madsens <shiloh.madsen@gmail.com>
2015-11-28 17:17:31 -0800

    Fixed namespacing

--- SantasWindow.py
+++ SantasWindow.py
@@ -13,11 +13,11 @@ while True:
     #If between 6AM and 5PM, Play Daytime
     if datetime.time(6,00) <= now.time() <= datetime.time(17,00):
         print "Its Daytime"
-        #Movies.StopLoop()
-        #Movies.StartLoop('/home/pi/Halloween2015/Assets/DayLoop')
+        Movies.StopLoop()
+        Movies.StartLoop('/home/pi/Halloween2015/Assets/DayLoop')
     else:
         print "Its Night Time"
-        #Movies.StopLoop()
-        #Movies.StartLoop('/home/pi/Halloween2015/Assets/NightLoop')
+        Movies.StopLoop()
+        Movies.StartLoop('/home/pi/Halloween2015/Assets/NightLoop')
     print "Sleeping for one hour"
     time.sleep(3600)
