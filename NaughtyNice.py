18bc08ea92fcbc27a7c1ecb1b09c0e62ed1a4a90
madsens <shiloh.madsen@gmail.com>
2015-11-28 20:07:55 -0800

    Trying to fade in text.

--- NaughtyNice.py
+++ NaughtyNice.py
@@ -12,6 +12,7 @@ pygame.display.init()
 # Display Parchment
 background = pygame.image.load ("/home/pi/Christmas-2015/Assets/brown-parchment.jpg")
 screen = pygame.display.set_mode (background.get_size())
+font = pygame.font.SysFont('sans-serif', 160, True)
 screen.blit (background, (0,0) )
 pygame.display.flip()
 
@@ -22,13 +23,23 @@ while True:
     # If STOP, Quit the program
     if rfid == "STOP":
         pygame.quit()
+        break
     else:
         # On Input, Disable Reader
         os.system("/home/pi/Christmas-2015/Scripts/disableRFID.sh")
 
-# Get the Name from the Scanned ID. For now just pass ID
+        # Get the Name from the Scanned ID. For now just pass ID
+        name = rfid
 
-# Fadein Name of scanned ID
+        # Fadein Name of scanned ID
+        label = font.render(name,1,(255,0,0))
+        newSurf = pygame.Surface(font.size(name))
+        newSurf.blit(label,(0,0))
+        for x in range (225):
+            newSurf.set_alpha(x - 255)
+            screen.blit(newSurf, (100,100))
+            pygame.display.flip()
+            pygame.time.delay(20)
 
 # Fade In Happy/Sad Claus
 
