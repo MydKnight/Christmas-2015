486a8a1bf470c87548cfaa38057daceff025c625
madsens <shiloh.madsen@gmail.com>
2015-11-28 16:51:46 -0800

    Santa's Window Gag

--- 1 - Santas Window.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'madsens'
-import Movies
-
-print 'Starting'
-Movies.StartLoop('/home/pi/Halloween2015/Assets/LivingLogo')
---
+++ Santas Window.py
@@ -0,0 +1,21 @@
+__author__ = 'madsens'
+import Movies
+from datetime import datetime, time
+
+print 'Starting'
+Movies.StartLoop('/home/pi/Halloween2015/Assets/LivingLogo')
+
+while True:
+    now = datetime.now()
+    now_time = now.time()
+    #If between 6AM and 5PM, Play Daytime
+    if time(6,00) <= now.time() <= time(17,00):
+        print "Its Daytime"
+        Movies.StopLoop()
+        Movies.StartLoop('/home/pi/Halloween2015/Assets/DayLoop')
+    else:
+        print "Its Night Time"
+        Movies.StopLoop()
+        Movies.StartLoop('/home/pi/Halloween2015/Assets/NightLoop')
+    print "Sleeping for one hour"
+    time.sleep(3600)
