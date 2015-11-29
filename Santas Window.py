16cc92b6704760b9cff74688ec6e21ecc1123fb5
madsens <shiloh.madsen@gmail.com>
2015-11-28 16:56:05 -0800

    Namespacing problems

--- Santas Window.py
+++ Santas Window.py
@@ -18,4 +18,4 @@ while True:
         #Movies.StopLoop()
         #Movies.StartLoop('/home/pi/Halloween2015/Assets/NightLoop')
     print "Sleeping for one hour"
-    time.sleep(3600)
+    time.time.sleep(3600)
