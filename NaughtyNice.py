bba068b1723536f67a31add8455b6c55da4ff1d8
madsens <shiloh.madsen@gmail.com>
2015-11-28 19:40:55 -0800

    Trying to display parchment.

--- NaughtyNice.py
+++ NaughtyNice.py
@@ -4,6 +4,17 @@ import pygame
 import Lights
 #import TextFade
 Lights.setup()
+
+pygame.init()
+clock = pygame.time.Clock()
+size = width, height = 1024, 768
+screen = pygame.display.set_mode(size)
+font = pygame.font.SysFont('serif', 160, True)
+fps_font = pygame.font.SysFont('serif', 20, True)
+bg = pygame.image.load("/home/pi/Christmas-2015/Assets/brown-parchment.jpg")
+
+while 1:
+    screen.blit(bg,(0,0))
 # This runs the Naughty/Nice Gag
 # Display Parchment
 
