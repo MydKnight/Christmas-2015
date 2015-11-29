9f76c14a777f79bef6463b6a723c5448a53ca8ae
madsens <shiloh.madsen@gmail.com>
2015-11-28 19:13:55 -0800

    Dining room gag

---
+++ NaughtyNice.py
@@ -0,0 +1,28 @@
+__author__ = 'madsens'
+
+import pygame
+import Lights
+import TextFade
+Lights.setup()
+# This runs the Naughty/Nice Gag
+# Display Parchment
+
+# Wait for Input
+
+# On Input, Disable Reader
+
+# Get the Name from the Scanned ID. For now just pass ID
+
+# Fadein Name of scanned ID
+
+# Fade In Happy/Sad Claus
+
+# Activate Naughty or Nice Pin
+
+# Wait
+
+# Fade Out Both
+
+# Turn off GPIO Pin
+
+# Reactivate Reader
