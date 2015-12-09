90e2b2e9c1d5acb5aea6c3d26d02ca0220376fd8
Shiloh Madsen <shiloh.madsen@evergegroup.com>
2015-12-08 23:54:27 -0600

    Trying to get the font in the right place

--- NaughtyNice/node.lua
+++ NaughtyNice/node.lua
@@ -1,6 +1,6 @@
 gl.setup(1024, 768)
 
-local font = resource.load_font("/home/pi/.fonts/Carrington.ttf")
+local font = resource.load_font("Carrington.ttf")
 
 function node.render()
     font:write(150, 320, "Hello World", 100, 1,1,1,1)
