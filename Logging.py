009da1d413fa86e11305915ee77c329f7b69c406
madsens <shiloh.madsen@gmail.com>
2015-12-29 13:37:30 -0800

    attempting to add row.

--- Logging.py
+++ Logging.py
@@ -34,8 +34,8 @@ def HeartBeat():
         #Get the IP address of the unit.
         ip = get_ip_address('wlan0')
         print ip
-        #res = cur.execute("INSERT INTO PIS (Status, InstallDate, IPAddress, MacAddress) VALUES (1,%s,%s, %s, %s);",(installDate,ip, mac))
-        #print res
+        res = cur.execute("INSERT INTO PIS (Status, InstallDate, IPAddress, MacAddress) VALUES (1,%s,%s, %s, %s);",(installDate,str(ip), str(mac)))
+        print res
     else:
         print "Row Found. Need to update row."
     #ToDo: Allow remote naming of PI by MAC Address.
