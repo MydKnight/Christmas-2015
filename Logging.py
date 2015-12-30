c9d040b3ca79c1b830f628186e556d1f9b389b02
madsens <shiloh.madsen@gmail.com>
2015-12-29 16:11:02 -0800

    Still fiddling with datatypes

--- Logging.py
+++ Logging.py
@@ -46,7 +46,7 @@ def HeartBeat():
         cur.execute("SELECT PIID FROM PIS WHERE MacAddress = %s;",str(mac))
         piid = cur.fetchone()[0]
         print type(piid)
-        res = cur.execute("""INSERT INTO Activity (ActivationTime, ActivationType, PIID) VALUES ("%s", 1, %s);""", (heartbeat, piid))
+        res = cur.execute("""INSERT INTO Activity (ActivationTime, ActivationType, PIID) VALUES (%s, 1, %s);""", (heartbeat, piid))
         print res
 
     #ToDo: Allow remote naming of PI by MAC Address.
